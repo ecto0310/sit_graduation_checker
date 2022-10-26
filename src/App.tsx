4b812e902b2b2d42efe0a20c0f164cecde9c9fbc
ecto0310 <ecto0310@gmail.com>
2022-10-26 22:32:38 +0900

    [add] alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Checker from "./Components/Checker";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
+import Alert from 'react-bootstrap/Alert';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavigationBar from "./Components/NavigationBar";
@@ -11,6 +12,9 @@ function App() {
     <div className="App">
       <NavigationBar />
       <Container>
+      <Alert variant={"danger"} className="mt-1">
+        使用は参考程度にしてください．万が一誤りがあった場合でも，製作者は一切責任を負いません．
+      </Alert>
         <BrowserRouter>
           <Routes>
             <Route path="/usage" element={<Usage />} />
