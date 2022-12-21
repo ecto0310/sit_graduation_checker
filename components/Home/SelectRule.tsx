2101dbaf4e1ff222a0e302b8460233b59752b69b
ecto0310 <ecto0310@gmail.com>
2022-12-21 17:13:08 +0900

    [fix] import

--- components/Home/SelectRule.tsx
+++ components/Home/SelectRule.tsx
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import { Department, RuleFile, PresetRuleFiles, Year } from '../../types/Rules/Rules';
+import { Department, RuleFile, PresetRuleFiles, Year } from '../../types/PresetRuleFiles';
 
 const SelectRule = () => {
     const router = useRouter();
