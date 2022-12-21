22d20b7a805fa747e192f3adbccf528ffa124ae1
ecto0310 <ecto0310@gmail.com>
2022-12-21 15:43:26 +0900

    [add] required subject info

--- components/Check/Rule/MinimumCreditRule.tsx
+++ components/Check/Rule/MinimumCreditRule.tsx
@@ -8,18 +8,22 @@ type MinimumCreditRuleProps = {
     passGrade: string[];
 }
 
-export const CalcMinimumCreditRule = (rule: MinimumCreditRule, credits: Credit[], passGrade: string[]): [boolean, number] => {
+export const CalcMinimumCreditRule = (rule: MinimumCreditRule, credits: Credit[], passGrade: string[]): [boolean, number, Credit[]] => {
     const passCredits = credits.filter((credits) => passGrade.includes(credits.grade));
     const targetCredits = passCredits.filter((credit) => rule.targets.some((target) => target.groups.includes(credit.group) && target.divisions.includes(credit.division)));
     const sumCredit = targetCredits.reduce((sum, targetCredit) => sum + targetCredit.count, 0);
 
     const result = rule.minimum <= sumCredit;
 
-    return [result, sumCredit];
+    return [result, sumCredit, passCredits];
 }
 
 const MinimumCreditRule = ({ rule, credits, passGrade }: MinimumCreditRuleProps) => {
-    const [result, sumCredit] = CalcMinimumCreditRule(rule, credits, passGrade);
+    const [result, sumCredit, passCredits] = CalcMinimumCreditRule(rule, credits, passGrade);
+    const nonPassrequiredSubjects = rule.requiredSubjects?.
+        filter((requiredSubject) =>
+            !passCredits.some((passCredit) => passCredit.name === requiredSubject)
+        )
 
     return (
         <>
@@ -27,7 +31,14 @@ const MinimumCreditRule = ({ rule, credits, passGrade }: MinimumCreditRuleProps)
                 <td>{rule.description}</td>
                 <td>{rule.minimum}</td>
                 <td>{sumCredit}</td>
-                <td>{Math.max(0, rule.minimum - sumCredit)}</td>
+                <td>
+                    {Math.max(0, rule.minimum - sumCredit)}
+                    {nonPassrequiredSubjects?.
+                        map((requiredSubject) =>
+                            <ul>{requiredSubject}</ul>
+                        )
+                    }
+                </td>
                 <td>{result ? "OK" : "NG"}</td>
             </tr>
         </>
