424435aeba60a56d500a9193ad49c604b4ad3a3b
ecto0310 <ecto0310@gmail.com>
2023-01-25 22:29:49 +0900

    [add] taking all pass

--- components/Check/Rule/MinimumGPARule.tsx
+++ components/Check/Rule/MinimumGPARule.tsx
@@ -8,20 +8,21 @@ type MinimumGPARuleProps = {
     rule: MinimumGPARule;
     credits: Credit[];
     gradePoint: GradePoint[];
+    isSchedule: boolean;
 }
 
-export const CalcMinimumGPARule = (rule: MinimumGPARule, credits: Credit[], gradePoint: GradePoint[]): [Result, number] => {
+export const CalcMinimumGPARule = (rule: MinimumGPARule, credits: Credit[], gradePoint: GradePoint[], isSchedule: boolean): [Result, number] => {
     const includeCredits = credits.filter((credit) => gradePoint.find((gradePoint) => gradePoint.grade == credit.grade) !== undefined);
     const creditCount = includeCredits.reduce((sum, credit) => sum + credit.count, 0);
     const sumGP = includeCredits.reduce((sum, credit) => sum + gradePoint.find((gradePoint) => gradePoint.grade == credit.grade)!.point * credit.count, 0);
     const GPA = Math.round(sumGP / creditCount * 10) / 10;
-    const result = rule.minimum <= GPA ? "pass" : "fail";
+    const result = isSchedule ? "unknown" : (rule.minimum <= GPA ? "pass" : "fail");
 
     return [result, GPA];
 }
 
-const MinimumGPARule = ({ rule, credits, gradePoint }: MinimumGPARuleProps) => {
-    const [result, GPA] = CalcMinimumGPARule(rule, credits, gradePoint);
+const MinimumGPARule = ({ rule, credits, gradePoint, isSchedule }: MinimumGPARuleProps) => {
+    const [result, GPA] = CalcMinimumGPARule(rule, credits, gradePoint, isSchedule);
 
     return (
         <>
